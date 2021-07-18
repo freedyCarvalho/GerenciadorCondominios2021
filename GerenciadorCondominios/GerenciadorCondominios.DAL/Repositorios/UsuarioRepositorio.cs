3740f9bc58c7bf4d95c6349bcb562c264854da6b
Freedy Carvalho <freedy.carvalho@mk2.com.br>
2021-07-18 18:16:25 -0300

    Registro de usuários parte 3

--- GerenciadorCondominios/GerenciadorCondominios.DAL/Repositorios/UsuarioRepositorio.cs
+++ GerenciadorCondominios/GerenciadorCondominios.DAL/Repositorios/UsuarioRepositorio.cs
@@ -1,15 +1,74 @@
 ﻿using GerenciadorCondominios.BLL.Models;
 using GerenciadorCondominios.DAL.Interface;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominios.DAL.Repositorios
 {
     public class UsuarioRepositorio : RepositorioGenerico<Usuario>, IUsuarioRepositorio
     {
-        public UsuarioRepositorio(Contexto contexto) : base(contexto)
+        private readonly Contexto _contexto;
+        private readonly UserManager<Usuario> _gerenciadorUsuarios;
+        private readonly SignInManager<Usuario> _gerenciadorLogin;
+
+        public UsuarioRepositorio(Contexto contexto, UserManager<Usuario> gerenciadorUsuarios, SignInManager<Usuario> gerenciadorLogin) : base(contexto)
         {
+            _contexto = contexto;
+            _gerenciadorLogin = gerenciadorLogin;
+            _gerenciadorUsuarios = gerenciadorUsuarios;
+        }
+
+        public async Task<IdentityResult> CriarUsuario(Usuario usuario, string senha)
+        {
+            try
+            {
+                return await _gerenciadorUsuarios.CreateAsync(usuario, senha);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public async Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao)
+        {
+            try
+            {
+                await _gerenciadorUsuarios.AddToRoleAsync(usuario, funcao);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public async Task LogarUsuario(Usuario usuario, bool lembrar)
+        {
+            try
+            {
+                await _gerenciadorLogin.SignInAsync(usuario, lembrar);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public int VerificarSeExisteRegistro()
+        {
+            try
+            {
+                return _contexto.Usuarios.Count();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
         }
     }
 }
