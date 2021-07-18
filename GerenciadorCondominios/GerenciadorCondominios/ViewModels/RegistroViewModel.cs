b537a7932512d55c83c6daa685371736a533b00e
Freedy Carvalho <freedy.carvalho@mk2.com.br>
2021-07-18 17:21:47 -0300

    Registros de usuários Parte 1

---
+++ GerenciadorCondominios/GerenciadorCondominios/ViewModels/RegistroViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominios.ViewModels
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use, no máximo, 40 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string  CPF { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Telefone { get; set; }
+
+
+        public string Foto { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use, no máximo, 40 caracteres")]
+        [EmailAddress(ErrorMessage = "E-mail inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use, no máximo, 40 caracteres")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(40, ErrorMessage = "Use, no máximo, 40 caracteres")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme a senha")]
+        [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+        public string SenhaConfirmada { get; set; }
+    }
+}
