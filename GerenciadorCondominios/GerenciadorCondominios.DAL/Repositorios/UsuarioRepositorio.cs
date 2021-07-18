56518d393faad5f1df0d965536ea6e6da9900b63
Freedy Carvalho <freedy.carvalho@mk2.com.br>
2021-07-18 17:49:07 -0300

    Registros de usuários parte 2

---
+++ GerenciadorCondominios/GerenciadorCondominios.DAL/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using GerenciadorCondominios.BLL.Models;
+using GerenciadorCondominios.DAL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorCondominios.DAL.Repositorios
+{
+    public class UsuarioRepositorio : RepositorioGenerico<Usuario>, IUsuarioRepositorio
+    {
+        public UsuarioRepositorio(Contexto contexto) : base(contexto)
+        {
+        }
+    }
+}
